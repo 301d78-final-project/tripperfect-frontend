1e62c1e643c1f6b146ff11ea1bab84f575ea0db4
Ediberto Ponce <ediberto01@gmail.com>
2021-10-07 15:01:20 -0700

    Added modal for events from API. Needs a bit more work.

--- src/Components/EventModal.js
+++ src/Components/EventModal.js
@@ -1,16 +1,35 @@
 import { Component } from 'react';
-import Container from 'react-bootstrap/Container';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
 
 class EventModal extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showModal: false,
-    };
-  }
+
+  
 
   render() {
-    return <></>;
+    console.log(this.props)
+    return (
+      
+      <Modal show={this.props.displayModal} onHide={this.props.onHide}>
+        <Modal.Dialog>
+          <Modal.Header closeButton>
+            <Modal.Title>{this.props.selectedEvent.name}</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            {/* <img src={this.props.selectedEvent.images[0].url} className="img-fluid" alt={''}></img> */}
+            {/* <p>{this.props.selectedEvent['dates']['start'].localDate}</p> */}
+          </Modal.Body>
+
+          <Modal.Footer>
+            {/* <p>{this.props.dates.start.localDate}</p> */}
+            <Button variant="secondary" onClick={this.props.onHide}>Close</Button>
+          </Modal.Footer>
+        </Modal.Dialog>
+      </Modal>
+     
+    )
   }
 }
 
