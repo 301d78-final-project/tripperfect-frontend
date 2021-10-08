b73c2c419e2c4261b67a545ff5f7168df54dc87b
Cameron Walden <cameronkwalden@gmail.com>
2021-10-07 21:00:43 -0700

    removes col for deployment purposes

--- src/Components/AboutUs.js
+++ src/Components/AboutUs.js
@@ -1,7 +1,6 @@
 import { Component } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import bios from '../teamBios.json'
 import AuthorCard from './AuthorCard'
 
