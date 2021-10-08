540a8d721ec826e2a90a5f10e54234af71f265d3
KATYROFFE <KATYROFFE@GMAIL.COM>
2021-10-07 21:37:14 -0700

    About us page corrections.

--- src/Components/AboutUs.js
+++ src/Components/AboutUs.js
@@ -1,9 +1,6 @@
 import { Component } from 'react';
-import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import { render } from '@testing-library/react';
 import bios from '../teamBios.json'
 import AuthorCard from './AuthorCard'
 // import devInfo from '../teamBios.json';
@@ -39,110 +36,5 @@ class AboutUs extends Component {
     );
   }
 }
-
-
-
-
-// class AboutUs extends Component {
-//   render() {
-//     return (
-//       <Container
-//         style={{
-//           display: 'flex',
-//           justifyContent: 'center',
-//           alignItems: 'center',
-//         }}
-//       >
-//         <Row>
-//           <Col>
-//             <Card style={{ width: '18rem' }}>
-//               <Card.Img variant='top' src={this.props.devInfo.authors[0].img} />
-//               <Card.Body>
-//                 <Card.Title>{this.props.devInfo.authors[0].name}</Card.Title>
-//                 {console.log(this.props.devInfo)}
-//                 <Card.Text>{this.props.devInfo.authors[0].bio}</Card.Text>
-//                 <Card.Link href={this.props.devInfo.authors[0].linkedin}>
-//                   LinkedIn
-//                 </Card.Link>
-//                 <Card.Link href={this.props.devInfo.authors[0].github}>
-//                   GitHub
-//                 </Card.Link>
-//               </Card.Body>
-//             </Card>
-//           </Col>
-
-//           <Col>
-//             <Card style={{ width: '18rem' }}>
-//               <Card.Img variant='top' src={this.props.devInfo.authors[1].img} />
-//               <Card.Body>
-//                 <Card.Title>{this.props.devInfo.authors[1].name}</Card.Title>
-//                 {console.log(this.props.devInfo)}
-//                 <Card.Text>{this.props.devInfo.authors[1].bio}</Card.Text>
-//                 <Card.Link href={this.props.devInfo.authors[1].linkedin}>
-//                   LinkedIn
-//                 </Card.Link>
-//                 <Card.Link href={this.props.devInfo.authors[1].github}>
-//                   GitHub
-//                 </Card.Link>
-//               </Card.Body>
-//             </Card>
-//           </Col>
-//         </Row>
-//         <Row>
-//           <Col>
-//             <Card style={{ width: '18rem' }}>
-//               <Card.Img variant='top' src={this.props.devInfo.authors[2].img} />
-//               <Card.Body>
-//                 <Card.Title>{this.props.devInfo.authors[2].name}</Card.Title>
-//                 {console.log(this.props.devInfo)}
-//                 <Card.Text>{this.props.devInfo.authors[2].bio}</Card.Text>
-//                 <Card.Link href={this.props.devInfo.authors[2].linkedin}>
-//                   LinkedIn
-//                 </Card.Link>
-//                 <Card.Link href={this.props.devInfo.authors[2].github}>
-//                   GitHub
-//                 </Card.Link>
-//               </Card.Body>
-//             </Card>
-//           </Col>
-
-//           <Col>
-//             <Card style={{ width: '18rem' }}>
-//               <Card.Img variant='top' src={this.props.devInfo.authors[3].img} />
-//               <Card.Body>
-//                 <Card.Title>{this.props.devInfo.authors[3].name}</Card.Title>
-//                 {console.log(this.props.devInfo)}
-//                 <Card.Text>{this.props.devInfo.authors[3].bio}</Card.Text>
-//                 <Card.Link href={this.props.devInfo.authors[3].linkedin}>
-//                   LinkedIn
-//                 </Card.Link>
-//                 <Card.Link href={this.props.devInfo.authors[3].github}>
-//                   GitHub
-//                 </Card.Link>
-//               </Card.Body>
-//             </Card>
-//           </Col>
-
-//           <Col>
-//             <Card style={{ width: '18rem' }}>
-//               <Card.Img variant='top' src={this.props.devInfo.authors[4].img} />
-//               <Card.Body>
-//                 <Card.Title>{this.props.devInfo.authors[4].name}</Card.Title>
-//                 {console.log(this.props.devInfo)}
-//                 <Card.Text>{this.props.devInfo.authors[4].bio}</Card.Text>
-//                 <Card.Link href={this.props.devInfo.authors[4].linkedin}>
-//                   LinkedIn
-//                 </Card.Link>
-//                 <Card.Link href={this.props.devInfo.authors[4].github}>
-//                   GitHub
-//                 </Card.Link>
-//               </Card.Body>
-//             </Card>
-//           </Col>
-//         </Row>
-//       </Container>
-//     );
-//   }
-// }
 
 export default AboutUs;
