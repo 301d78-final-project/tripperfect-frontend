c335c0731bfa710e3e7c9bdf1fbe6b336fe39e1b
Cameron <cameronkwalden@gmail.com>
2022-05-25 16:58:26 -0700

    moves Card to EventCard, still fixing Modal functionality

---
+++ src/Components/EventCard.js
@@ -0,0 +1,90 @@
+import { Component } from "react";
+import { withAuth0 } from "@auth0/auth0-react";
+import { Card, Button, Row } from "react-bootstrap";
+// import EventModal from "./EventModal";
+
+class EventCard extends Component {
+  render() {
+    return (
+      <>
+        <Row
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          {this.props.eventData.map((attraction) => (
+            <Card key={attraction.id} id="eventcard" style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={attraction.images[0].url} />
+              <Card.Body>
+                <Card.Title>{attraction.name}</Card.Title>
+                <Card.Text>
+                  <a
+                    href={attraction.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Ticket Link
+                  </a>
+                </Card.Text>
+                <Card.Text>
+                  <strong>Min Price:</strong> ${attraction.priceRanges[0].min} |{" "}
+                  <strong>Max Price:</strong> ${attraction.priceRanges[0].max}
+                </Card.Text>
+                <Card.Text>
+                  <strong>Start Date:</strong>{" "}
+                  {attraction.dates.start.localDate} |{" "}
+                  <strong>Local Start Time:</strong>{" "}
+                  {attraction.dates.start.localTime}
+                </Card.Text>
+                <Button onClick={() => this.props.showModal(attraction.name)}>
+                  More Info
+                </Button>
+                {/* <EventModal
+                  selectedEvent={this.props.selectedEvent}
+                  displayModal={this.props.displayModal}
+                  onHide={this.props.hideModal}
+                /> */}
+
+                {this.props.auth0.isAuthenticated ? (
+                  <Button
+                    id="eventbutton"
+                    variant="primary"
+                    onClick={() => this.props.addEvents(attraction.name)}
+                  >
+                    Save Event!
+                  </Button>
+                ) : (
+                  <Button id="eventbutton" variant="primary">
+                    Log In To Save Event
+                  </Button>
+                )}
+              </Card.Body>
+            </Card>
+          ))}
+        </Row>
+        <Row
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Card style={{ width: "30rem" }} id="welcome-card">
+            <Card.Img
+              variant="top"
+              src="./images/TripPerfect2.png"
+              alt="welcome to TripPerfect"
+            />
+            <Card.Body>
+              <Card.Title>WELCOME!</Card.Title>
+            </Card.Body>
+          </Card>
+        </Row>
+      </>
+    );
+  }
+}
+
+export default withAuth0(EventCard);
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,15 +1,12 @@
 import { Component } from "react";
-import Container from "react-bootstrap/Container";
-// import Form from "react-bootstrap/Form";
-import Card from "react-bootstrap/Card";
-import Row from "react-bootstrap/Row";
-// import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
 import axios from "axios";
-import "../css/EventCards.css";
+// import { Container, Card, Row, Button } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import { withAuth0 } from "@auth0/auth0-react";
-import EventModal from "./EventModal";
+// import EventModal from "./EventModal";
+import EventCard from "./EventCard";
 import EventForm from "./EventForm";
+import "../css/EventCards.css";
 
 class Home extends Component {
   constructor(props) {
@@ -56,87 +53,16 @@ class Home extends Component {
         <EventForm getEvents={this.props.getEvents} 
         setSearchQuery={this.props.setSearchQuery}
         />
-
-        <Row
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          {this.props.eventData.map((attraction) => (
-            <Card key={attraction.id} id="eventcard" style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={attraction.images[0].url} />
-              <Card.Body>
-                <Card.Title>{attraction.name}</Card.Title>
-                <Card.Text>
-                  <a
-                    href={attraction.url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Ticket Link
-                  </a>
-                </Card.Text>
-                <Card.Text>
-                  <strong>Min Price:</strong> ${attraction.priceRanges[0].min} |{" "}
-                  <strong>Max Price:</strong> ${attraction.priceRanges[0].max}
-                </Card.Text>
-                <Card.Text>
-                  <strong>Start Date:</strong>{" "}
-                  {attraction.dates.start.localDate} |{" "}
-                  <strong>Local Start Time:</strong>{" "}
-                  {attraction.dates.start.localTime}
-                </Card.Text>
-                <Button onClick={() => this.showModal(attraction.name)}>
-                  More Info
-                </Button>
-                <EventModal
-                  selectedEvent={this.state.selectedEvent}
-                  displayModal={this.state.displayModal}
-                  onHide={this.hideModal}
-                />
-
-                {this.props.auth0.isAuthenticated ? (
-                  <Button
-                    id="eventbutton"
-                    variant="primary"
-                    onClick={() => this.addEvents(attraction.name)}
-                  >
-                    Save Event!
-                  </Button>
-                ) : (
-                  <Button id="eventbutton" variant="primary">
-                    Log In To Save Event
-                  </Button>
-                )}
-              </Card.Body>
-            </Card>
-          ))}
-          {/* </Col> */}
-        </Row>
-        <Row
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          {/* <Col> */}
-          <Card style={{ width: "30rem" }} id="welcome-card">
-            <Card.Img
-              variant="top"
-              src="./images/TripPerfect2.png"
-              alt="welcome to TripPerfect"
-            />
-            <Card.Body>
-              <Card.Title>WELCOME!</Card.Title>
-            </Card.Body>
-          </Card>
-          {/* </Col> */}
-        </Row>
+        <EventCard 
+        eventData={this.props.eventData}
+        showModal={this.showModal}
+        hideModal={this.hideModal}
+        // selectedEvent={this.state.selectedEvent}
+        // displayModal={this.state.displayModal}
+        // addEvents={this.addEvents}
+        />
       </Container>
     );
   }
 }
-export default withAuth0(Home);
+export default Home;
