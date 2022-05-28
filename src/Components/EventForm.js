75526150311a4419360cd549cd1ee2862c27a5f7
Cameron <cameronkwalden@gmail.com>
2022-05-28 12:14:14 -0700

    refactors to functional component, changes submit button to light

--- src/Components/EventForm.js
+++ src/Components/EventForm.js
@@ -1,32 +1,24 @@
-import { Component } from "react";
-import{ Form, Row, Col, Button } from 'react-bootstrap';
+import { Form, Row, Col, Button } from "react-bootstrap";
 
-export default class EventForm extends Component {
-  render() {
-    return (
-      <>
-        <Form onSubmit={(event) => this.props.getEvents(event)}>
-          <Row className="justify-content-md-center">
-            <Col sm={3} className="my-1">
-              <Form.Label htmlFor="inlineFormInputName" visuallyHidden>
-                Please Enter City Name
-              </Form.Label>
-              <Form.Control
-                onChange={(e) => this.props.setSearchQuery(e.target.value)}
-                placeholder="Ex: Seattle"
-              />
-            </Col>
-            <Col xs="auto" className="my-1">
-              <Button
-                type="submit"
-                as="input"
-                value="Submit"
-                variant="primary"
-              />{" "}
-            </Col>
-          </Row>
-        </Form>
-      </>
-    );
-  }
+export default function EventForm(props) {
+  return (
+    <>
+      <Form onSubmit={(event) => props.getEvents(event)}>
+        <Row className="justify-content-md-center">
+          <Col sm={3} className="my-1">
+            <Form.Label htmlFor="inlineFormInputName" visuallyHidden>
+              Please Enter City Name
+            </Form.Label>
+            <Form.Control
+              onChange={(e) => props.setSearchQuery(e.target.value)}
+              placeholder="Ex: Seattle"
+            />
+          </Col>
+          <Col xs="auto" className="my-1">
+            <Button type="submit" as="input" value="Submit" variant="light" />{" "}
+          </Col>
+        </Row>
+      </Form>
+    </>
+  );
 }
