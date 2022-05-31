7e913178dac0fc433d3e1ed325ce38c3c146ab7c
Cameron <cameronkwalden@gmail.com>
2022-05-30 20:12:29 -0700

    refactors to functional component

--- src/App.js
+++ src/App.js
@@ -1,73 +1,64 @@
-import React from 'react';
-import axios from 'axios';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { withAuth0 } from '@auth0/auth0-react';
-import Header from './components/Header';
-import Home from './components/home/Home';
-import Login from './components/Login';
-import SavedEvents from './components/savedevents/SavedEvents';
-import AboutUs from './components/AboutUs';
-import Footer from './components/footer/Footer';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { withAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
+import Header from "./components/Header";
+import Home from "./components/home/Home";
+import Login from "./components/Login";
+import AboutUs from "./components/AboutUs";
+import SavedEvents from "./components/savedevents/SavedEvents";
+import Footer from "./components/footer/Footer";
+import "./App.css";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchQuery: '',
-      eventData: [],
-      error: false
-    };
-  }
+function App(props) {
+  let [searchQuery, setSearchQuery] = useState('');
+  let [eventData, setEventData] = useState([]);
 
-  setSearchQuery = searchQuery => this.setState({ searchQuery: searchQuery });
+  const handleSearchQuery = (searchQuery) =>
+    setSearchQuery(searchQuery);
 
-  getEvents = async event => {
+  const getEvents = async (event) => {
     event.preventDefault();
     try {
-      const eventAPI = `http://localhost:3001/events?city=${this.state.searchQuery}&startDateTime`;
+      const eventAPI = `http://localhost:3001/events?city=${searchQuery}&startDateTime`;
       const eventResponse = await axios.get(eventAPI);
-
-      console.log(eventResponse, '<== eventResponse')
-      console.log(eventResponse.data._embedded.events,'<== eventResponse.data._embedded.events');
-      this.setState({ eventData: eventResponse.data._embedded.events });
+      setEventData(eventData = eventResponse.data._embedded.events);
     } catch (error) {
-      this.setState({ error: true });
+      console.log(error)
     }
   };
 
-  render() {
-    return (
-      <>
-        <Router>
-          <Header />
-          <Switch>
-            <Route path='/saved-events'>
-              {this.props.auth0.isAuthenticated ? (
-                <SavedEvents
-                  eventData={this.state.eventData}
-                  getEvents={this.getEvents}
-                />
-              ) : (
-                <Login />
-              )}
-            </Route>
-            <Route exact path='/'>
-              <Home
-                setSearchQuery={this.setSearchQuery}
-                eventData={this.state.eventData}
-                getEvents={this.getEvents}
+  return (
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/saved-events">
+            {props.auth0.isAuthenticated ? (
+              <SavedEvents
+                eventData={eventData}
+                getEvents={getEvents}
               />
-            </Route>
-            <Route path='/about-us'>
-              <AboutUs/>
-            </Route>
-          </Switch>
-          <Footer />
-        </Router>
-      </>
-    );
-  }
+            ) : (
+              <Login />
+            )}
+          </Route>
+          <Route exact path="/">
+            <Home
+              handleSearchQuery={handleSearchQuery}
+              eventData={eventData}
+              getEvents={getEvents}
+            />
+          </Route>
+          <Route path="/about-us">
+            <AboutUs />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </>
+  );
 }
 
 export default withAuth0(App);
